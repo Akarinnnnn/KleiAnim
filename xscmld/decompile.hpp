96b62d40b13afd3121c3a26999219d1971638b68
Fa鸽 <1207247008@qq.com>
2020-01-31 21:41:43 +0800

    移动xscml的代码

--- xscmld/decompile.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <filesystem>
-namespace KleiAnim::Tool
-{
-	__declspec(dllexport) void decompile_scml(std::filesystem::path animation, std::filesystem::path build, float framerate = 30.0);
-}

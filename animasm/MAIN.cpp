fb25a42e661aa2f81e3d4dc6e111aad4bf614390
Fa鸽 <43724908+Akarinnnnn@users.noreply.github.com>
2022-08-30 11:50:51 +0800

    更正被包含文件的路径

--- animasm/MAIN.cpp
+++ animasm/MAIN.cpp
@@ -1,7 +1,7 @@
 ﻿// animc.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
 //
 #include <iostream>
-#include "../Decompiler/arg_parser.h"
+#include "../animdasm/arg_parser.h"
 #include "../klei_anim/KleiAnim/Xml.hpp"
 #include <filesystem>
 using std::filesystem::path;
